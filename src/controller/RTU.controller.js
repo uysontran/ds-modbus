2df0ad1a0aa8984f49038742d7bf611488b1b617
Miles <tranuyson@gmail.com>
2022-03-02 22:37:22 +0700

    add parser option

--- src/controller/RTU.controller.js
+++ src/controller/RTU.controller.js
@@ -2,6 +2,7 @@ const PQueue = require("p-queue");
 const queue = new PQueue({ concurrency: 1 });
 const { SerialPort } = require("serialport");
 const Modbus = require("jsmodbus");
+const { Buffer } = require("buffer");
 function rtu(req, res) {
   const {
     host,
@@ -50,11 +51,21 @@ function rtu(req, res) {
 }
 class RTU {
   async rtu(req, res) {
-    const { path } = req.body;
+    const { path, parse, parser, channel_name } = req.body;
     try {
       let result = await queue.add(() => rtu(req, res));
-      return res.send(result.response._body);
+      let buf = Buffer.from(result.response._body._valuesAsBuffer);
+      if (parse === "key/value") {
+        return res.send({
+          [channel_name]:
+            JSON.parse(parser)[result.response._body._values] ||
+            result.response._body._values,
+        });
+      } else {
+        return res.send({ [channel_name]: buf[`read${parse}`](0) });
+      }
     } catch (err) {
+      console.log(err.message);
       if (err.message === `Opening ${path}: Access denied`) {
         return res.sendStatus(503);
       } else {
