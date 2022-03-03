d1f25d363f9709a95a283e90d3f292000c7a3916
Miles <tranuyson@gmail.com>
2022-03-03 12:21:55 +0700

    update new tcp code

--- src/controller/TCP.controller.js
+++ src/controller/TCP.controller.js
@@ -2,7 +2,7 @@ const Modbus = require("jsmodbus");
 const net = require("net");
 const PQueue = require("p-queue");
 const queue = new PQueue({ concurrency: 1 });
-function tcp(req, res) {
+function tcp(req) {
   const { host, port, id, fc, addr, quantity, value } = req.body;
   return new Promise((resovle, reject) => {
     const socket = new net.Socket();
@@ -33,11 +33,27 @@ function tcp(req, res) {
 }
 class TCP {
   async tcp(req, res) {
+    const { path, parse, parser, channel_name } = req.body;
     try {
       let result = await queue.add(() => tcp(req, res));
-      return res.json(result.response._body._values);
+      let buf = Buffer.from(result.response._body._valuesAsBuffer);
+      if (parse === "key/value") {
+        return res.send({
+          [channel_name]:
+            JSON.parse(parser)[result.response._body._values] ||
+            result.response._body._values,
+        });
+      } else {
+        return res.send({ [channel_name]: buf[`read${parse}`](0) });
+      }
     } catch (err) {
-      return res.sendStatus(404);
+      console.log(err.message);
+      if (err.message === `Opening ${path}: Access denied`) {
+        return res.sendStatus(503);
+      } else {
+        console.log(err);
+        return res.sendStatus(404);
+      }
     }
   }
 }
--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -3,7 +3,9 @@ const express = require("express");
 module.exports = function (app) {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
-  if (process.env.dev === "true") {
-    app.use(require("morgan")("combined"));
-  }
+  app.use(
+    require("morgan")(
+      "':method :url :status :res[content-length] - :response-time ms'"
+    )
+  );
 };
