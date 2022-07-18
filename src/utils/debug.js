0490dad81ba0cfee9e22356e9110b8268d46d240
miles <tranuyson@gmail.com>
2022-07-18 16:55:09 +0700

    delete some unused code

--- src/utils/debug.js
+++
@@ -1,8 +0,0 @@
-const DEBUG = require("debug");
-const debug = DEBUG("ds-modbus");
-debug.log = console.log.bind(console);
-DEBUG.enable("ds-modbus:*");
-module.exports = function (type) {
-  return (log) =>
-    debug.extend(type)(`${process.memoryUsage().rss / (1024 * 1024)}: ${log}`);
-};
